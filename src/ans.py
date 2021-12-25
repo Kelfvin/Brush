297942d1540da0b19879e259e0b5b18d2b8ae21c
kevin feng <494407383@qq.com>
2021-12-25 16:34:13 +0800

    update

--- src/ans.py
+++ src/ans.py
@@ -8,72 +8,82 @@ import src.trace as trace
 def ans():
 
     file_list = os.listdir(r'./bank/')
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print('题库列表：')
 
-    n = 0 
+    n = 0
     for name in file_list:
-        section = re.findall(r'(.*)\.md',name)[0]
-        print('[{}].'.format(n),section,trace.get_process(section))
-        n+=1
+        section = re.findall(r'(.*)\.md', name)[0]
+        print('[{}].'.format(n), section, trace.get_process(section))
+        n += 1
 
     choice = input('\n请输入你要进行的章节选项(输入u返回上一级菜单,q退出)[u/q]：')
 
-    if(choice == 'u' or choice == 'U'):
+    if (choice == 'u' or choice == 'U'):
         return
 
-    elif(choice == 'q' or choice == 'Q'):
+    elif (choice == 'q' or choice == 'Q'):
         exit()
 
     else:
-        file = open('./bank/'+file_list[int(choice)],mode='rt',encoding='utf-8')
+        file = open('./bank/' + file_list[int(choice)],
+                    mode='rt',
+                    encoding='utf-8')
         content = file.read()
-        content = re.sub(r'###','*****\n###',content)
-        content = re.sub(r'$','\n*****',content)
-        title = re.findall(r'# (.*)\n',content)[0]
+        content = re.sub(r'###', '*****\n###', content)
+        content = re.sub(r'$', '\n*****', content)
+        title = re.findall(r'# (.*)\n', content)[0]
 
-        problems = re.findall(r'### (?P<problems>.*?)\n',content)
-        options = re.findall(r'### .*?\n(?P<options>[\s\S]*?)\*\*\*\*\*',content)
-        section = re.findall(r'(.*)\.md',file_list[int(choice)])[0]
-
+        problems = re.findall(r'### (?P<problems>.*?)\n', content)
+        options = re.findall(r'### .*?\n(?P<options>[\s\S]*?)\*\*\*\*\*',
+                             content)
+        section = re.findall(r'(.*)\.md', file_list[int(choice)])[0]
 
         # 查找记录，开始记录位置遍历题目
-        for index in range(trace.get_trace(section),len(problems)):
-            trace.store_trace(section,index)
-            os.system('cls')
-            print(title,'\n')
-            print('({}/{})'.format(index+1,len(problems)))
+        for index in range(trace.get_trace(section), len(problems)):
+            trace.store_trace(section, index)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(title, '\n')
+            print('({}/{})'.format(index + 1, len(problems)))
             print()
 
-            type = re.findall(r'\((?P<type>\S+)\)',problems[index])[0]
-            key = re.findall(r'正确答案: (.+?)\n',options[index])[0]
+            type = re.findall(r'\((?P<type>\S+)\)', problems[index])[0]
+            key = re.findall(r'正确答案: (.+?)\n', options[index])[0]
 
-            
-            options[index]=re.sub(r'正确答案.*\n','',options[index])
+            options[index] = re.sub(r'正确答案.*\n', '', options[index])
             print(problems[index])
             print(options[index])
 
             answer = input('请输入你的答案(输入u返回上一级,q退出)：')
-            if(answer=='Q' or answer == 'q'):
+            answer = answer.replace(' ', '')
+            answer = answer.upper()
+
+            if type == '判断题':
+                if answer == 'A':
+                    answer = '对'
+
+                elif answer == 'B':
+                    answer = '错'
+
+            if (answer == 'Q' or answer == 'q'):
                 exit()
 
-            elif(answer=='u' or answer=='U'):
+            elif (answer == 'u' or answer == 'U'):
                 return
 
-            elif(answer == key):
+            elif (answer == key):
                 print('正确！')
                 fault_note.rmove_fault_note(title)
 
             else:
-                print('错误！  正确答案：',key)
-                fault_note.add_fault_note(problems[index],options[index],key)
-
+                print('错误！  正确答案：', key)
+                fault_note.add_fault_note(problems[index], options[index], key)
 
             input("输入任意键继续：")
-            os.system('cls')
+            os.system('cls' if os.name == 'nt' else 'clear')
 
-        trace.store_trace(section,0)
-        choice = input('还需要继续吗？\n[任意键]继续\n[q]退出')
+        trace.store_trace(section, 0)
+        choice = input('该章所有题目已经完成！\n是否还要继续？\n[任意键]返回主菜单\n[q]退出')
 
-        if(choice =='q'):
+        if (choice == 'q'):
             exit()
--- src/fault_note.py
+++ src/fault_note.py
@@ -4,9 +4,10 @@ import pickle
 
 
 def solve_no_note():
-    if(not os.path.isfile('./fault_note/fault_note')):
-        file = open('./fault_note/fault_note','wb')
+    if (not os.path.isfile('./fault_note/fault_note')):
+        file = open('./fault_note/fault_note', 'wb')
         file.close()
+
 
 def add_fault_note(title, options, key):
     solve_no_note()
@@ -23,8 +24,9 @@ def add_fault_note(title, options, key):
         file_in.close()
 
         file_out = open('./fault_note/fault_note', mode='wb')
-        pickle.dump(fault_dic,file_out)
+        pickle.dump(fault_dic, file_out)
         file_out.close()
+
 
 def rmove_fault_note(title):
     solve_no_note()
@@ -34,16 +36,17 @@ def rmove_fault_note(title):
     if (len(content)):
         fault_dic = pickle.loads(content)
 
-    if(title in fault_dic):
+    if (title in fault_dic):
         del fault_dic[title]
         file_in.close()
 
         file_out = open('./fault_note/fault_note', mode='wb')
-        pickle.dump(fault_dic,file_out)
-        file_out.close()    
-    
+        pickle.dump(fault_dic, file_out)
+        file_out.close()
+
+
 def review_fault():
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     solve_no_note()
     file_in = open('./fault_note/fault_note', mode='rb')
     content = file_in.read()
@@ -52,44 +55,46 @@ def review_fault():
         fault_dic = pickle.loads(content)
 
     times = 0
-    for title,opt_key in fault_dic.items():
-        times+=1
+    for title, opt_key in fault_dic.items():
+        times += 1
         options = opt_key['options']
         key = opt_key['key']
 
-        os.system('cls')
+        os.system('cls' if os.name == 'nt' else 'clear')
         print('错题回顾')
-        print('({}/{})'.format(times,len(fault_dic)))
+        print('({}/{})'.format(times, len(fault_dic)))
         print(title)
-        print(opt_key['options'],'\n')
+        print(opt_key['options'], '\n')
 
         answer = input('请输入你的答案(输入u返回上一级,q退出)：')
-        if(answer=='q' or answer == 'Q'):
+        answer = answer.replace(' ', '')
+        answer = answer.upper()
+        if (answer == 'q' or answer == 'Q'):
             exit()
 
-        elif(answer=='u'or answer=='U'):
+        elif (answer == 'u' or answer == 'U'):
             return
 
-        elif(answer==key):
+        elif (answer == key):
             print('正确！')
             rmove_fault_note(title)
 
         else:
-            print('错误！  正确答案：',key)
-            add_fault_note(title,options,key)
+            print('错误！  正确答案：', key)
+            add_fault_note(title, options, key)
 
         input("输入任意键继续：")
-        os.system('cls')
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-        
-    choice = input('错题做完了！\n[任意键]退出\n[u]返回主菜单\n请输入你的选择[*|q]:')
+    choice = input('错题做完了！\n[q]退出\n[u]返回主菜单\n请输入你的选择[*|q]:')
 
-    if(choice =='u' or choice == 'U'):
+    if (choice == 'u' or choice == 'U'):
         return
 
-    else:
+    elif (choice == 'q' or choice == 'Q'):
         exit()
-        
+
+
 def get_fault_note_numbers():
     solve_no_note()
     file_in = open('./fault_note/fault_note', mode='rb')
@@ -97,7 +102,7 @@ def get_fault_note_numbers():
     fault_dic = {}
     if (len(content)):
         fault_dic = pickle.loads(content)
-        num =  len(fault_dic)
+        num = len(fault_dic)
         return '({}道错题)'.format(num)
 
     else:

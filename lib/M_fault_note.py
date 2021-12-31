d75e0135da7e2c51549d421da2039d1c2246387b
kevin feng <494407383@qq.com>
2021-12-31 11:05:00 +0800

    update

--- lib/M_fault_note.py
+++ lib/M_fault_note.py
@@ -2,6 +2,7 @@
 import os
 import re
 import pickle
+import random
 
 
 base_path = './data/fault_note/'
@@ -21,7 +22,8 @@ def add_fault_note(file_name,title, type, options, key):
         fault_dic = pickle.loads(content)
 
     if (title not in fault_dic):
-        temp = {'type': type, 'options': options, 'key': key}
+        temp = {'type': type, 'options': options, 'key': key , 'review_times':0}
+        # review_times 该错题复习的次数
         fault_dic[title] = temp
 
         file_in.close()
@@ -38,9 +40,16 @@ def rmove_fault_note(file_name,title):
     fault_dic = {}
     if (len(content)):
         fault_dic = pickle.loads(content)
+        review_times = fault_dic[title]['review_times']
 
-    if (title in fault_dic):
-        del fault_dic[title]
+    if title in fault_dic:
+        if fault_dic[title]['review_times'] == 3:
+        # 错题复习了4次就删除
+            del fault_dic[title]
+
+        else: 
+            fault_dic[title]['review_times'] = review_times+1
+
         file_in.close()
 
         file_out = open(f'{base_path}{file_name}', mode='wb')
@@ -57,6 +66,8 @@ def review_fault(file_name):
     fault_dic = {}
     if (len(content)):
         fault_dic = pickle.loads(content)
+        fault_dic = random_dic(fault_dic)
+        # 将错题字典顺序打乱，做错题时不按照顺序
 
     times = 0
     for title, opt_key in fault_dic.items():
@@ -64,10 +75,13 @@ def review_fault(file_name):
         type = opt_key['type']
         options = opt_key['options']
         key = opt_key['key']
+        review_times = opt_key['review_times']
+        # review_times 还需要复习的次数
 
         os.system('cls' if os.name == 'nt' else 'clear')
         print('错题回顾')
         print('({}/{})'.format(times, len(fault_dic)))
+        print(f'此题记忆进度 ({review_times}/{3})')
         print(title)
         print(opt_key['options'], '\n')
 
@@ -124,3 +138,12 @@ def get_fault_note_numbers(file_name):
 
 def reset_fault_note(file_name):
     os.remove(f'{base_path}{file_name}')
+
+
+def random_dic(dict):
+    dict_key_ls = list(dict.keys())
+    random.shuffle(dict_key_ls)
+    new_dic = {}
+    for key in dict_key_ls:
+        new_dic[key] = dict[key]
+    return new_dic
